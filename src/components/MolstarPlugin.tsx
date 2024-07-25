beebe25d59d88be79436b02e59910b36be60bd51
kcaj21 <127600321+kcaj21@users.noreply.github.com>
2024-07-25 17:50:23 +0100

    works on vm

--- src/components/MolstarPlugin.tsx
+++ src/components/MolstarPlugin.tsx
@@ -5,18 +5,50 @@ import { PluginConfig } from 'molstar/lib/mol-plugin/config';
 import { renderReact18 } from 'molstar/lib/mol-plugin-ui/react18';
 import { PluginUIContext } from "molstar/lib/mol-plugin-ui/context";
 import "molstar/lib/mol-plugin-ui/skin/light.scss";
+import { PluginSpec } from 'molstar/lib/mol-plugin/spec';
+import { PluginBehaviors } from 'molstar/lib/mol-plugin/behavior';
+import { PluginLayoutControlsDisplay } from 'molstar/lib/mol-plugin/layout';
 // import { Sequence } from 'molstar/lib/mol-model/sequence';
 
+const defaultSpec = DefaultPluginUISpec()
+
 const MySpec: PluginUISpec = {
-    ...DefaultPluginUISpec(),
+  actions: defaultSpec.actions,
+  behaviors: [
+      PluginSpec.Behavior(PluginBehaviors.Representation.HighlightLoci, { mark: false }),
+      PluginSpec.Behavior(PluginBehaviors.Representation.DefaultLociLabelProvider),
+      PluginSpec.Behavior(PluginBehaviors.Camera.FocusLoci),
+
+      PluginSpec.Behavior(PluginBehaviors.CustomProps.StructureInfo),
+      PluginSpec.Behavior(PluginBehaviors.CustomProps.Interactions),
+      PluginSpec.Behavior(PluginBehaviors.CustomProps.SecondaryStructure),
+  ],
+  animations: defaultSpec.animations,
+  customParamEditors: defaultSpec.customParamEditors,
+  layout: {
+      initial: {
+          isExpanded: false,
+      showControls: true,
+      // controlsDisplay: 'reactive' as PluginLayoutControlsDisplay //adding this seems maybe to make the view window reactive to the setting panel
+      },
+  },
+  components: {
+      ...defaultSpec.components,
+      controls: {
+          ...defaultSpec.components?.controls,
+          top: 'none',
+          bottom: 'none',
+          left: 'none',
+      },
+      remoteState: 'none'
+  },
     config: [
       [PluginConfig.VolumeStreaming.Enabled, false],
       [PluginConfig.Viewport.ShowAnimation, false],
-      [PluginConfig.Viewport.ShowSettings, false],
+      [PluginConfig.Viewport.ShowSettings, true],
       [PluginConfig.Viewport.ShowExpand, false],
-      [PluginConfig.Viewport.ShowControls, false],
+      [PluginConfig.Viewport.ShowControls, true],
       [PluginConfig.Viewport.ShowTrajectoryControls, false]
-
 
     ]
 }
