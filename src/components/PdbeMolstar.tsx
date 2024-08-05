9b9964145c23656239e5be0be1db2b60f72c8484
kcaj21 <alex.kcaj@gmail.com>
2024-08-05 12:55:05 +0100

    changed how plugin is initialized, less confusing and no type errors with options object

--- src/components/PdbeMolstar.tsx
+++ src/components/PdbeMolstar.tsx
@@ -1,52 +1,57 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, createRef } from 'react';
 import { PDBeMolstarPlugin } from 'pdbe-molstar/lib';
+import { InitParams } from 'pdbe-molstar/lib/spec';
 
-const MolstarViewer = () => {
-  const viewerContainerRef = useRef<HTMLDivElement>(null);
 
-  const [options, setOptions] = useState({
-    customData: {
-      url: 'https://alphafold.ebi.ac.uk/files/AF-Q5VSL9-F1-model_v4.pdb',
-          format: 'pdb',
-      binary: false
-    },
-    alphafoldView: true,
-    bgColor: { r: 0, g: 0, b: 0 },
-    hideCanvasControls: [
-      'selection',
-      'animation',
-    ],
-    sequencePanel: true,
-      landscape: false,
-      reactive: true,
-      controlsDisplay: "reactive",
+interface PDBeMolStarWrapperProps {
 
-      hideControls: false
-  });
+  onInit: (instance: PDBeMolstarPlugin) => void
+}
 
+const MolstarViewer: React.FC<PDBeMolStarWrapperProps> = ({ onInit }) => {
+  const viewerContainerRef = createRef<HTMLDivElement>()
+
+  // In debug mode of react's strict mode, this code will
+  // be called twice in a row, which might result in unexpected behavior.
   useEffect(() => {
-    const viewerInstance = new PDBeMolstarPlugin();
-    viewerInstance.render(viewerContainerRef.current!, options);
-    viewerInstance.visual.toggleSpin(true)
-      
-  }, [options]);
+    function init() {
+      const url = `https://alphafold.ebi.ac.uk/files/AF-Q5VSL9-F1-model_v4.pdb`
 
-  const handleClick = () => {
-    setOptions(({
-      ...options,
-      sequencePanel: !options.sequencePanel
-    }));
-  };
-    
+      const pluginInstance = new PDBeMolstarPlugin()
 
+
+      //Set options (Checkout available options list in the documentation)
+      const options: Partial<InitParams> = {
+        customData: {
+          url: url,
+          format: "pdb",
+          binary: false,
+        },
+        alphafoldView: true,
+        bgColor: { r: 0, g: 0, b: 0 },
+        // hideControls: true,
+        sequencePanel: true,
+        reactive: true
+      }
+
+      if (viewerContainerRef.current === null) return
+
+      //Call render method to display the 3D view
+      pluginInstance.render(viewerContainerRef.current, options)
+
+      if (onInit) {
+        onInit(pluginInstance)
+      }
+    }
+    init()
+  }, [])
 
   return (
     <>
         <div className="relative h-[80%] w-full max-height-full max-width-full  ">
               <div ref={viewerContainerRef} >  
               </div>
-        </div>
-      
+        </div>   
     </>
   );
 };
