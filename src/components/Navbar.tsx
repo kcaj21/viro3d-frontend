8695eb0cee06593fbd35b87e8bac2f16fdb343f5
admin account <viro-admin@viro3d-dev.cvr.gla.ac.uk>
2024-08-07 14:51:47 +0100

    proteinInfo component in reasonable condition

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar: React.FC = () => {
 
   return (
     <>
-      <nav className='fixed top-0 left-0 right-0 z-20 border-b-2 text-[#4a95c0] drop-shadow-md bg-[#e6e6e6]'>
+      <nav className='fixed top-0 left-0 right-0 z-20 border-b-2 border-[#d6d5d5] text-[#4a95c0] drop-shadow-md bg-[#e6e6e6]'>
         <div className='flex items-center md:justify-center sm: justify-end mx-auto px-4 py-2'>
           <ul className='flex p-4 md:p-0 md:flex-row md:space-x-32 font-extralight text-4xl text-[#4a95c0]'>
             <li>
