abf7b32f63017d4c750a9c018017dd333208addb
admin account <viro-admin@viro3d-dev.cvr.gla.ac.uk>
2024-10-02 11:30:17 +0100

    changed results cards design to be less boring

--- src/components/ProteinStructureResults.tsx
+++ src/components/ProteinStructureResults.tsx
@@ -2,60 +2,81 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ProteinStructureResults: React.FC = ({ data, filterParam }) => {
-
-  return filterParam !== 'sequencematch' ?
-    (
+  return filterParam !== "sequencematch" ? (
     <>
       <ul className="px-8 py-8 grid grid-cols-2 gap-4">
         {data.protein_structures?.map((protein) => (
           <Link
-            to={{ pathname: `/structureindex/${protein['Virus name(s)']}/${protein._id}` }}
+            to={{
+              pathname: `/structureindex/${protein["Virus name(s)"]}/${protein._id}`,
+            }}
             key={protein._id}
           >
-            <div className="result-card drop-shadow-md lg:min-h-[15vh] lg:max-h-[15vh] 2xl:min-h-[10vh] 2xl:max-h-[10vh] flex flex-row-2 gap-2 border-2 hover:border-[#4a95c0] rounded-md bg-[#f9f9f9]">
-              <div className="basis-1/2 px-6 py-2">
-                <li className="  text-xl text-[#4a95c0] font-light">
-                  {protein.genbank_name}
-                </li>
+            <div className="result-card hover:-translate-y-1 transition ease-in-out drop-shadow-md sm:min-h-[10vh] sm:max-h-[10vh] lg:min-h-[15vh] lg:max-h-[15vh] 2xl:min-h-[15vh] 2xl:max-h-[15vh] flex flex-row justify-between border-2 border-[#4a95c0] hover:border-[#50bde5] rounded-md bg-[#f9f9f9]">
+              <div className="px-4 py-2 flex flex-col gap-2 basis-2/3">
+                <div className="">
+                  <li className="2xl:text-2xl lg:text-xl sm:text-sm text-[#4a95c0] font-light">
+                    {protein.genbank_name.substring(0, 70)}
+                  </li>
+                </div>
+                <div className="sm:hidden xl:block lg:text-base 2xl:text-xl font-thin">
+                  {/* <li className=""> Product: {protein.product}</li> */}
+                  <li className="">Peptide Category: {protein.pept_cat}</li>
+                </div>
               </div>
-              <div className="lg:text-base 2xl:text-xl basis-1/2 py-2 font-thin">
-                <li className="">
+              <div className="basis-1/3 flex flex-col justify-end ">
+                <li className="lg:text-sm 2xl:text-2xl sm:text-xs text-white font-thin sm:py-1 sm:px-1 lg:px-2 lg:py-2 bg-[#4a95c0]  ">
                   pLDDT Score: {protein.colabfold_json_pLDDT}
                 </li>
-                <li className=""> Product: {protein.product}</li>
-                <li className=""> Species: {protein.Species}</li>
-              </div>
-            </div>
-          </Link>
-        ))}
-        </ul>
-    </>
-    ) : (
-      <>
-      <ul className="px-8 py-8 grid grid-cols-2 gap-4">
-        {data.matches?.map((match) => (
-          <Link
-            to={{ pathname: `/structureindex/${match.protein_structure['Virus name(s)']}/${match.protein_structure._id}` }}
-            key={match.protein_structure._id}
-          >
-            <div onClick={() => handleMatchClick(match.protein_structure['Virus name(s)'])} className="result-card min-h-[10vh] drop-shadow-md  flex flex-col-2 gap-4 border-2 hover:border-[#4a95c0] rounded-md   bg-[#f9f9f9]">
-              <div className="basis-1/4  px-4 py-2">
-                <li className=" text-[#4a95c0] font-light">
-                  {match.protein_structure.protein_id}
-                </li>
-                <li className=" text-lg text-[#4a95c0] font-light">Blastp Score: {match.score}</li>
-              </div>
-              <div className="basis-3/4 px-4 py-2 text-xl font-thin">
-                <li className="">plDDT Score: {match.protein_structure.colabfold_json_pLDDT}</li>
-                <li className=""> Product: {match.protein_structure.product}</li>
-                <li className=""> Species: {match.protein_structure.Species}</li>
               </div>
             </div>
           </Link>
         ))}
       </ul>
-    </>  
-  )
+    </>
+  ) : (
+    <>
+      <ul className="px-8 py-8 grid grid-cols-2 gap-4">
+        {data.matches?.map((match) => (
+          <Link
+            to={{
+              pathname: `/structureindex/${match.protein_structure["Virus name(s)"]}/${match.protein_structure._id}`,
+            }}
+            key={match.protein_structure._id}
+          >
+            <div
+              onClick={() =>
+                handleMatchClick(match.protein_structure["Virus name(s)"])
+              }
+              className="result-card min-h-[10vh] drop-shadow-md  flex flex-col-2 gap-4 border-2 hover:border-[#4a95c0] rounded-md   bg-[#f9f9f9]"
+            >
+              <div className="basis-1/4  px-4 py-2">
+                <li className=" text-[#4a95c0] font-light">
+                  {match.protein_structure.protein_id}
+                </li>
+                <li className=" text-lg text-[#4a95c0] font-light">
+                  Blastp Score: {match.score}
+                </li>
+              </div>
+              <div className="basis-3/4 px-4 py-2 text-xl font-thin">
+                <li className="">
+                  plDDT Score: {match.protein_structure.colabfold_json_pLDDT}
+                </li>
+                <li className="">
+                  {" "}
+                  Product: {match.protein_structure.product}
+                </li>
+                <li className="">
+                  {" "}
+                  Species: {match.protein_structure.Species}
+                </li>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default ProteinStructureResults;
